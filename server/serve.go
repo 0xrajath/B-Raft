c279c946dd407a1fbd6eceeaa87eb9ef9ccb86bc
rajathalex <rajathalex@gmail.com>
2018-11-06 04:57:51 +0000

    Leader election done

--- server/serve.go
+++ server/serve.go
@@ -48,8 +48,8 @@ func (r *Raft) RequestVote(ctx context.Context, arg *pb.RequestVoteArgs) (*pb.Re
 // Compute a random duration in milliseconds
 func randomDuration(r *rand.Rand) time.Duration {
 	// Constant
-	const DurationMax = 4000
-	const DurationMin = 1000
+	const DurationMax = 20000
+	const DurationMin = 5000
 	return time.Duration(r.Intn(DurationMax-DurationMin)+DurationMin) * time.Millisecond
 }
 
@@ -68,6 +68,20 @@ func restartTimer(timer *time.Timer, r *rand.Rand) {
 	timer.Reset(randomDuration(r))
 }
 
+// Stop the supplied timer - For use when Candidate becomes Leader after majority votes
+func stopTimer(timer *time.Timer) {
+	stopped := timer.Stop()
+	// If stopped is false that means someone stopped before us, which could be due to the timer going off before this,
+	// in which case we just drain notifications.
+	if !stopped {
+		// Loop for any queued notifications
+		for len(timer.C) > 0 {
+			<-timer.C
+		}
+
+	}
+}
+
 // Restart the heartbeat timer
 func restartHBTimer(timer *time.Timer) {
 	stopped := timer.Stop()
@@ -80,7 +94,21 @@ func restartHBTimer(timer *time.Timer) {
 		}
 
 	}
-	timer.Reset(100 * time.Millisecond)
+	timer.Reset(1000 * time.Millisecond)
+}
+
+// Stop the heartbeat timer - For use when Leader goes back to being Follower
+func stopHBTimer(timer *time.Timer) {
+	stopped := timer.Stop()
+	// If stopped is false that means someone stopped before us, which could be due to the timer going off before this,
+	// in which case we just drain notifications.
+	if !stopped {
+		// Loop for any queued notifications
+		for len(timer.C) > 0 {
+			<-timer.C
+		}
+
+	}
 }
 
 // Launch a GRPC service for this Raft peer.
@@ -151,15 +179,15 @@ func serve(s *KVStore, r *rand.Rand, peers *arrayPeers, id string, port int, tot
 
 	// Create a timer and start running it
 	timer := time.NewTimer(randomDuration(r))
-	//To Do: assign this only when leader is elected
-	heartbeatTimer := time.NewTimer(100 * time.Millisecond)
+	// Creating heartbeatTimer - This ensures it's not called before the first election timeout
+	heartbeatTimer := time.NewTimer(100000 * time.Millisecond)
 
-	// State -- To add more terms
 	//log.Printf("Total number of nodes : %v", totNumNodes)
+	// State -- To add more terms
 	var currentTerm int64 //Default is 0
-	votes := 0
+	var votes int
 	var votedFor string
-	//var currentLeader string
+	var currentLeader string
 
 	// Run forever handling inputs from various channels
 	for {
@@ -167,6 +195,7 @@ func serve(s *KVStore, r *rand.Rand, peers *arrayPeers, id string, port int, tot
 		case <-timer.C:
 			// The Election timer went off - Convert to candidate
 			log.Printf("Election Timeout!! - Convert to Candidate")
+			votes = 0 //Resets vote count
 
 			//Election
 			currentTerm++
@@ -184,19 +213,21 @@ func serve(s *KVStore, r *rand.Rand, peers *arrayPeers, id string, port int, tot
 			// This will also take care of any pesky timeouts that happened while processing the operation.
 			restartTimer(timer, r)
 		case <-heartbeatTimer.C:
-			//Heartbeats
-			log.Printf("Sending heartbeats from leader:%v in term:%v", id, currentTerm)
+			if id == currentLeader { //Only run this if it is current leader
+				//Heartbeats
+				log.Printf("Sending heartbeats from leader:%v,%v in term:%v", id, currentLeader, currentTerm)
 
-			for p, c := range peerClients {
-				// Send in parallel so we don't wait for each client.
-				go func(c pb.RaftClient, p string) {
-					ret, err := c.AppendEntries(context.Background(), &pb.AppendEntriesArgs{Term: currentTerm, LeaderID: id})
-					appendResponseChan <- AppendResponse{ret: ret, err: err, peer: p}
-				}(c, p)
+				for p, c := range peerClients {
+					// Send in parallel so we don't wait for each client.
+					go func(c pb.RaftClient, p string) {
+						ret, err := c.AppendEntries(context.Background(), &pb.AppendEntriesArgs{Term: currentTerm, LeaderID: id})
+						appendResponseChan <- AppendResponse{ret: ret, err: err, peer: p}
+					}(c, p)
+				}
+
+				// This will also take care of any pesky timeouts that happened while processing the operation.
+				restartHBTimer(heartbeatTimer)
 			}
-
-			// This will also take care of any pesky timeouts that happened while processing the operation.
-			restartHBTimer(heartbeatTimer)
 		case op := <-s.C:
 			// We received an operation from a client
 			// TODO: Figure out if you can actually handle the request here. If not use the Redirect result to send the
@@ -212,12 +243,26 @@ func serve(s *KVStore, r *rand.Rand, peers *arrayPeers, id string, port int, tot
 			if currentTerm < ae.arg.Term {
 				log.Printf("Term incremented. My term: %v. Appender term: %v", currentTerm, ae.arg.Term)
 				currentTerm = ae.arg.Term
-				votedFor = "" //Resetting votedFor as I've not yet voted for anyone in this updated term
+				votedFor = ""            //Resetting votedFor as I've not yet voted for anyone in this updated term
+				votes = 0                //Resetting my vote count
+				if currentLeader == id { //If I am the leader
+					stopHBTimer(heartbeatTimer) //Since Leader stepping down to follower
+				}
+				currentLeader = ae.arg.LeaderID // Assigning new leader with the higher term
+				restartTimer(timer, r)
+			} else if currentTerm == ae.arg.Term {
+				log.Printf("Leader is %v for term %v", ae.arg.LeaderID, currentTerm)
+				currentLeader = ae.arg.LeaderID //Assigning leader for whom we voted earlier
+				//votes = 0                       //Required??
+				restartTimer(timer, r)
+			} else { //Receiving Stale Term
+				log.Printf("Append request from %v rejected as appender term < my term. My term: %v. Appender term: %v", ae.arg.LeaderID, currentTerm, ae.arg.Term)
+				ae.response <- pb.AppendEntriesRet{Term: currentTerm, Success: false}
 			}
 
-			ae.response <- pb.AppendEntriesRet{Term: 1, Success: true}
+			//ae.response <- pb.AppendEntriesRet{Term: 1, Success: true}
 			// This will also take care of any pesky timeouts that happened while processing the operation.
-			restartTimer(timer, r)
+
 		case vr := <-raft.VoteChan:
 			// We received a RequestVote RPC from a raft peer
 			// TODO: Fix this.
@@ -227,6 +272,10 @@ func serve(s *KVStore, r *rand.Rand, peers *arrayPeers, id string, port int, tot
 				if currentTerm < vr.arg.Term { //Current term is less than Requester term
 					currentTerm = vr.arg.Term
 					votedFor = vr.arg.CandidateID
+					votes = 0                //Reset my own votes incase I was a candidate
+					if currentLeader == id { //If I am the leader
+						stopHBTimer(heartbeatTimer) //Since Leader stepping down to follower
+					}
 					log.Printf("Voted for %v due to term increase", vr.arg.CandidateID)
 					vr.response <- pb.RequestVoteRet{Term: currentTerm, VoteGranted: true} //Voted for Requester
 					restartTimer(timer, r)
@@ -256,17 +305,63 @@ func serve(s *KVStore, r *rand.Rand, peers *arrayPeers, id string, port int, tot
 				// Do not do Fatalf here since the peer might be gone but we should survive.
 				log.Printf("Error calling RPC %v", vr.err)
 			} else {
-				log.Printf("Got response to vote request from %v", vr.peer)
-				log.Printf("Peers %s granted %v term %v", vr.peer, vr.ret.VoteGranted, vr.ret.Term)
+				//To check Term of response received
 
-				// //Vote Granted by peer
-				// if vr.ret.VoteGranted {
-				// 	votes++
-				// }
+				log.Printf("Got response to vote request from %v", vr.peer)
+				log.Printf("Peer %s granted %v term %v", vr.peer, vr.ret.VoteGranted, vr.ret.Term)
+
+				if currentTerm < vr.ret.Term { //Some other node is at a higher term - Candidate changes to Follower
+					log.Printf("Term incremented. My term: %v. Voter term: %v", currentTerm, vr.ret.Term)
+					log.Printf("Stepping down to Follower")
+					currentTerm = vr.ret.Term
+					votedFor = "" //Resetting votedFor as I've not yet voted for anyone in this updated term
+					votes = 0     //Resetting my vote count
+					restartTimer(timer, r)
+				} else if currentTerm == vr.ret.Term {
+					//Vote Granted by peer
+					if vr.ret.VoteGranted {
+						votes++
+						log.Printf("Peer %v voted %v in term %v. Vote Count: %v ", vr.peer, vr.ret.VoteGranted, currentTerm, votes)
+					}
+
+					if votes > (totNumNodes/2) && currentLeader != id { //Majority vote achieved - Candidate changes to Leader
+						log.Printf("Got Majority vote count of %v among %v nodes", votes, totNumNodes)
+						log.Printf("Converting to Leader from Candidate")
+						currentLeader = id                                      //Assigning self as Leader
+						stopTimer(timer)                                        //Stopping Election timer since it has become leader
+						heartbeatTimer = time.NewTimer(1000 * time.Millisecond) //Starting heartbeatTimer
+					}
+				} else { //Receiving Vote response for Stale Term
+					log.Printf("Do Nothing. Received vote response for stale term. My term: %v. Voter term: %v", currentTerm, vr.ret.Term)
+				}
+
 			}
 		case ar := <-appendResponseChan:
 			// We received a response to a previous AppendEntries RPC call
-			log.Printf("Got append entries response from %v", ar.peer)
+			if ar.err != nil {
+				// Do not do Fatalf here since the peer might be gone but we should survive.
+				log.Printf("Error calling RPC %v", ar.err)
+			} else {
+				log.Printf("Got append entries response from %v", ar.peer)
+				log.Printf("Peer %s granted Success: %v in term %v", ar.peer, ar.ret.Success, ar.ret.Term)
+
+				if currentTerm < ar.ret.Term { //Some other node is at a higher term - Leader changes to Follower
+					log.Printf("Term incremented. My term: %v. Appender term: %v", currentTerm, ar.ret.Term)
+					log.Printf("Stepping down to Follower")
+					currentTerm = ar.ret.Term
+					votedFor = "" //Resetting votedFor as I've not yet voted for anyone in this updated term
+					votes = 0     //Resetting my vote count
+					restartTimer(timer, r)
+					stopHBTimer(heartbeatTimer) //Since Leader stepping down to follower
+				} else if currentTerm == ar.ret.Term {
+					//Do something here - I'm guessing log replication happens here.
+					log.Printf("Some log replication I guess") //To change
+				} else { //Receiving Append response for Stale Term
+					log.Printf("Do Nothing. Received append response for stale term. My term: %v. Appender term: %v", currentTerm, ar.ret.Term)
+				}
+
+			}
+
 		}
 	}
 	log.Printf("Strange to arrive here")
